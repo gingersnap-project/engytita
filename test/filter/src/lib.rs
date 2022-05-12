5d9f87da36ba5ad5f2f5ac0bb155ce84b0341719
Tristan Tarrant <ttarrant@redhat.com>
2022-05-12 17:43:53 +0200

    Don't use Stretto for now

--- test/filter/src/lib.rs
+++ test/filter/src/lib.rs
@@ -1,16 +1,26 @@
 use log::*;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
-use stretto::Cache;
+use std::collections::HashMap;
+use std::sync::{Mutex, Arc, RwLock};
 
 proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
     proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { 
-        Box::new(EngytitaRoot {}) 
+        Box::new(EngytitaRoot {
+            cache: Arc::new(RwLock::new(HashMap::new()))
+        }) 
     });
 }}
 
-struct EngytitaRoot {}
+struct HttpResponse {
+    headers: Vec<(String, String)>,
+    body: String
+}
+
+struct EngytitaRoot {
+    cache: Arc<RwLock<HashMap<String, HttpResponse>>>
+}
 
 impl Context for EngytitaRoot {}
 
@@ -28,48 +38,36 @@ impl RootContext for EngytitaRoot {
 
     fn create_http_context(&self, _: u32) -> Option<Box<dyn HttpContext>> {
         Some(Box::new(EngytitaHttp {
-            //cache: HashMap::new(),
-            cache: Cache::new(12960, 1e6 as i64).unwrap()
+            cache: Arc::clone(&self.cache),
+            key: None,
+            headers: None
         }))
     }
 }
 
 struct EngytitaHttp {
-    cache: Cache<String, String>
+    cache: Arc<RwLock<HashMap<String, HttpResponse>>>,
+    key: Option<String>,
+    headers: Option<Vec<(String, String)>>
 }
 
 impl Context for EngytitaHttp {}
 
 impl HttpContext for EngytitaHttp {
     fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
-        // Print the headers
-        /*for (name, value) in &self.get_http_request_headers() {
-            info!("{}: {}", name, value);
-        }*/
-        info!("Cache size {}", self.cache.len());
         match self.get_http_request_header(":path") {
             Some(path) => {
                 match path.strip_prefix("/airports/") {
                     Some(airport) => {
                         info!("Request {}", airport);
-                        let airport = airport.to_owned();
-                        match self.cache.get(&airport) {
-                            Some(cached) => {
-                                info!("Cached {}", airport);
-                                // We have cached it
-                                self.send_http_response(
-                                    200,
-                                    vec![("X-Powered-By", "Engytita")],
-                                    Some(b"Cached!\n"),
-                                );
-                                Action::Pause
-                            }
-                            _ => {
-                                // Not cached yet
-                                info!("Caching {}", airport);
-                                self.cache.insert(airport, "Cached!\n".to_owned(),1);
-                                Action::Continue
-                            }
+                        let cache = self.cache.read().expect("RwLock poisoned");
+                        if let Some(cached) = cache.get(airport) {
+                            info!("Using cached {}", airport);
+                            Action::Continue
+                        } else {
+                            drop(cache);
+                            self.key.replace(airport.to_owned());
+                            Action::Continue
                         }
                         
                     }
@@ -80,32 +78,37 @@ impl HttpContext for EngytitaHttp {
         }
     }
 
-    fn on_http_response_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
-        if !end_of_stream {
-            return Action::Pause;
-        }
-        if let Some(body_bytes) = self.get_http_response_body(0, body_size) {
-            let body_str = String::from_utf8(body_bytes).unwrap();
-            info!("Body: {}", body_str);
-        }
-        return Action::Continue;
+    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
+        match self.key.as_ref() {
+            Some(key) => {
+                // The key matched, let's save the headers
+                self.headers.replace(self.get_http_response_headers().clone());
+                Action::Continue
+            }
+            _ => Action::Continue
+        }    
     }
 
-    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
-        for (name, value) in &self.get_http_response_headers() {
-            info!("{}: {}", name, value);
-        }
-        match self.get_http_response_header(":path") {
-            Some(path) => {
-                match path.strip_prefix("/airports/") {
-                    Some(airport) => {
-                        info!("Response {}", airport);
-                        Action::Continue
-                    }
-                    _ => Action::Continue
+    fn on_http_response_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
+        match self.key.as_ref() {
+            Some(key) => {
+                if !end_of_stream {
+                    // We need more data!
+                    return Action::Pause;
                 }
+                if let Some(body_bytes) = self.get_http_response_body(0, body_size) {
+                    let body_str = String::from_utf8(body_bytes).unwrap();
+                    info!("{} Body: {}", key, body_str);
+                    // TODO: store headers and body in the cache
+                }
+                Action::Continue
             }
-            _ => Action::Continue,
+            _ => {
+                // The key in this request didn't match our cache rule, just let it through as-is
+                info!("Passthrough");
+                Action::Continue
+            }
+
         }
     }
 }
